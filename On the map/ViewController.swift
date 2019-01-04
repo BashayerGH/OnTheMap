e6fd11538079460e1629769fbc840aa10257ce17
Bashooora <bashayeralghamdi@bashayers-macbook-pro.local>
2019-01-04 10:09:03 +0300

    Initial Commit

---
+++ On the map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On the map
+//
+//  Created by Bashooora on 28/04/1440 AH.
+//  Copyright © 1440 Bashayer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
